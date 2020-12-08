0bc70c07f1fd5a6bed22c6d1630f11acc165023c
fowlerro <kamilekwentka@gmail.com>
2020-12-09 00:23:13 +0100

    added backend

---
+++ backend/src/app.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+require('./strategies/discord');
+const express = require('express');
+const passport = require('passport');
+const app = express();
+const PORT = process.env.PORT || 3002;
+const routes = require('./routes');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const Store = require('connect-mongo')(session);
+
+mongoose.connect('mongodb+srv://dbUser:JtwwkIEaZukU2SE6@cluster0.wsvos.mongodb.net/Lunaris?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+});
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    cookie: {
+        maxAge: 60000 * 60 * 24 * 7
+    },
+    resave: false,
+    saveUninitialized: false,
+    store: new Store({mongooseConnection: mongoose.connection}),
+}))
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Running on Port ${PORT}`));
