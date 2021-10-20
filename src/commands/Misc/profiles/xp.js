3e4cd42250b31b80c2efb0547792b4f30f3e0e13
fowlerro <kamilekwentka@gmail.com>
2021-10-20 18:19:21 +0200

    xp command

--- src/commands/Misc/profiles/xp.js
+++ src/commands/Misc/profiles/xp.js
@@ -19,9 +19,12 @@ module.exports = {
     },
     category: 'profiles',
     syntax: {
-        pl: 'xp <user> <akcja> <ilość> [jednostka] [opcje]',
-        en: 'xp <user> <action> <amount> [unit] [options]',
+        pl: 'xp <user> <akcja> <typ> <ilość> [jednostka] [opcje]',
+        en: 'xp <user> <action> <type> <amount> [unit] [options]',
     },
+    // syntaxHelp: {
+    //     pl: `Akcja: get/add/remove/set\nJednostka: xp/lvl\n\nOpcje:\nglobal/server\n`
+    // },
     permissions: [],
     allowedChannels: [],
     blockedChannels: [],
@@ -35,8 +38,9 @@ module.exports = {
     cooldownRoles: [],
     cooldownReminder: true,
     async run(client, message, args) {
+        const typeArgs = { text: ['text'], voice: ['voice', 'vc'] }
         const unitArgs = { xp: ['xp'], lvl: ['lvl', 'level', 'levels'] }
-        const scopeArgs = { global: ['g', 'global'], guild: ['server', 'guild', 'local'] }
+        const scopeArgs = { global: ['g', 'global'], guild: ['server', 'serwer', 'guild', 'local'] }
         const [ user, action, ...options] = args
         // action: set, get, add, remove
         // unit: xp, lvl/level/levels
@@ -44,8 +48,12 @@ module.exports = {
         const member = !isNaN(user) ? await message.guild.members.fetch(user) : message.mentions.members.first();
         if(!member) return
 
-        const amount = ['set', 'add', 'remove'].includes(options[0]) ? options[0] : null
-        if(['set', 'add', 'remove'].includes(options[0])) options.shift()
+        const type = typeArgs.text.includes(options[0]) ? 'text' : typeArgs.voice.includes(options[0]) ? 'voice' : null
+        if(!type && action !== 'get') return
+        if(type) options.shift()
+
+        const amount = ['set', 'add', 'remove'].includes(action) ? options[0] : null
+        if(['set', 'add', 'remove'].includes(action)) options.shift()
 
         const unit = unitArgs.xp.includes(options[0]) ? 'xp' : unitArgs.lvl.includes(options[0]) ? 'lvl' : 'xp'
         if(unitArgs.xp.includes(options[0]) ||  unitArgs.lvl.includes(options[0])) options.shift()
@@ -59,44 +67,43 @@ module.exports = {
         const guild = await client.guilds.fetch(guildId).catch(() => {})
         if(scope === 'guild' && !guild) return
 
-        const msg = await handleAction(member.id, action, amount, unit, scope, guild)
-        console.log(msg)
+        const msg = await handleAction(member.id, type, action, amount, unit, scope, guild)
         return message.channel.send(msg);
     }
 }
 
-function handleAction(userId, action, amount, unit, scope, guild) {
-    if(action === 'add') return add(userId, amount, unit, scope, guild)
-    if(action === 'remove') return remove(userId, amount, unit, scope, guild)
-    if(action === 'set') return set(userId, amount, unit, scope, guild)
+function handleAction(userId, type, action, amount, unit, scope, guild) {
+    if(action === 'add') return add(userId, type, amount, unit, scope, guild)
+    if(action === 'remove') return remove(userId, type, amount, unit, scope, guild)
+    if(action === 'set') return set(userId, type, amount, unit, scope, guild)
     return get(userId, scope, guild)
 }
 
 async function get(userId, scope, guild) {
     const profile = await Profiles.get(global.client, userId, scope === 'global' ? null : guild?.id)
-    console.log(profile)
     const text = profile.statistics.text
     const voice = profile.statistics.voice
-    const msg = `Text: ${text.level} lvl | ${text.xp} xp | ${text.totalXp} total XP | ${text.dailyXp} daily XP
-Voice: ${voice.level} lvl | ${voice.xp} xp | ${voice.totalXp} total XP | ${voice.dailyXp} daily XP | ${voice.timeSpent} min`
 
     const embed = new MessageEmbed()
         .setColor(palette.info)
         .addField('Text', `${text.level} lvl\n${text.xp} xp\n${text.totalXp} total XP\n${text.dailyXp} daily XP`, true)
         .addField('Voice', `${voice.level} lvl\n${voice.xp} xp\n${voice.totalXp} total XP\n${voice.dailyXp} daily XP\n${voice.timeSpent} min`, true)
 
-    console.log(embed)
     return { embeds: [embed] }
 }
 
-function add(userId, amount, unit, scope, guild) {
-    console.log('add', { amount, unit, scope, guild })
+async function add(userId, type, amount, unit, scope, guild) {
+    console.log('add', { type, amount, unit, scope, guild })
 }
 
-function remove(userId, amount, unit, scope, guild) {
-    console.log('remove', { amount, unit, scope, guild })
+async function remove(userId, type, amount, unit, scope, guild) {
+    console.log('remove', { type, amount, unit, scope, guild })
 }
 
-function set(userId, amount, unit, scope, guild) {
-    console.log('set', { amount, unit, scope, guild })
+async function set(userId, type, amount, unit, scope, guild) {
+    console.log('set', { type, amount, unit, scope, guild })
+
+    const profile = await Profiles.get(global.client, userId, scope === 'global' ? null : guild?.id)
+    const stats = profile.statistics[type]
+    
 }
