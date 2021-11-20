cab0c684db7ded3fea74837d78ff80f89bd5b37c
fowlerro <kamilekwentka@gmail.com>
2021-11-20 16:39:57 +0100

    .

--- src/bot.js
+++ src/bot.js
@@ -1,10 +1,11 @@
 require('dotenv').config();
-const { Client, Intents, Collection } = require('discord.js');
+const { Client, Intents, Collection } = require('discord.js')
 const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_VOICE_STATES]});
 global.client = client
 const { connectDatabase } = require('./database/mongoose');
 const { registerCommands, registerEvents, registerModules } = require('./utils/registry');
 const dashboard = require('./dashboard/app');
+
 
 (async () => {
   await connectDatabase();
