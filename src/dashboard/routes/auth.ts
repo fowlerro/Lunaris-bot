d66a57d8d26ea31911eee5088982636819101ef8
fowlerro <fwlerro@gmail.com>
2022-01-14 03:06:20 +0100

    Clean up invite redirects

--- src/dashboard/routes/auth.ts
+++ src/dashboard/routes/auth.ts
@@ -14,18 +14,6 @@ router.get('/discord/redirect', passport.authenticate('discord'), (req, res) => 
     res.redirect('http://localhost:3000/servers');
 });
 
-// router.get('/discord/invite/redirect', (req, res) => {
-//     res.redirect(`http://localhost:3000/dashboard/${req.query.guild_id}`);
-// })
-
-// router.get('/discord/invite', (req, res) => {
-//     res.redirect('https://discord.com/api/oauth2/authorize?client_id=739412828737372181&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Finvite%2Fredirect&response_type=code&scope=bot%20guilds.join')
-// });
-
-// router.get('/discord/invite/:id', (req, res) => {
-//     res.redirect(`https://discord.com/api/oauth2/authorize?client_id=739412828737372181&permissions=8&redirect_uri=http%3A%2F%2Flocalhost%3A3001%2Fapi%2Fauth%2Fdiscord%2Finvite%2Fredirect&response_type=code&scope=bot%20guilds.join&guild_id=${req.params.id}`)
-// });
-
 router.get('/', (req, res) => {
     if(req.user) {
         res.send(req.user);
