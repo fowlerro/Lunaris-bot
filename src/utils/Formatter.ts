ea86ded8383abd938a52d42a45eaa1e9903cab51
fowlerro <fwlerro@gmail.com>
2022-01-19 18:06:15 +0100

    Added more formatters

--- src/utils/Formatter.ts
+++ src/utils/Formatter.ts
@@ -3,17 +3,91 @@ interface IVariables {
 }
 
 const supportedFormatters = [
+    // MEMBER
     {
         name: 'memberId', path: 'member.id',
-        description: "Member's identificator"
+        description: {
+            en: "Member's identificator",
+            pl: "Identyfikator użytkownika"
+        } 
     },
     {
         name: 'username', path: 'member.user.username',
-        description: "Member's username"
+        description: {
+            en: "Member's username",
+            pl: "Nazwa użytkownika"
+        }
     },
     {
         name: 'nickname', path: 'member.nickname',
-        description: "Member's nickname"
+        description: {
+            en: "Member's nickname, or username if doesn't have nickname",
+            pl: "Pseudonim użytkownika lub nazwa, jeśli nie posiada pseudonimu"
+        }
+    },
+    {
+        name: 'discriminator', path: 'member.user.discriminator',
+        description: {
+            en: "Tag numbers of the member",
+            pl: "4 cyfrowy tag użytkownika"
+        }
+    },
+    {
+        name: 'tag', path: 'member.user.tag',
+        description: {
+            en: "Member's username and discriminator (username#0000)",
+            pl: "Nazwa oraz tag użytkownika (username#0000)"
+        }
+    },
+    {
+        name: 'memberCreatedAt', path: 'member.user.createdAt',
+        description: {
+            en: "The date of the member's account creation",
+            pl: "Data stworzenia konta użytkownika"
+        }
+    },
+    // GUILD
+    {
+        name: 'serverCreatedAt', path: 'guild.createdAt',
+        description: {
+            en: "The date of the server creation",
+            pl: "Data stworzenia serwera"
+        }
+    },
+    {
+        name: 'serverId', path: 'guild.id',
+        description: {
+            en: "Server's identificator",
+            pl: "Identyfikator serwera"
+        }
+    },
+    {
+        name: 'serverMemberCount', path: 'guild.memberCount',
+        description: {
+            en: "Server members count",
+            pl: "Ilość użytkowników na serwerze"
+        }
+    },
+    {
+        name: 'serverName', path: 'guild.name',
+        description: {
+            en: "The name of this server",
+            pl: "Nazwa tego serwera"
+        }
+    },
+    {
+        name: 'serverPreferredLocale', path: 'guild.preferredLocale',
+        description: {
+            en: "Server's preferred locale",
+            pl: "Preferowany język serwera"
+        }
+    },
+    {
+        name: 'serverRulesChannelId', path: 'guild.rulesChannelId',
+        description: {
+            en: "Identificator of the rules channel in server",
+            pl: "Identyfikator kanału zasad na serwerze"
+        }
     }
 ]
 
