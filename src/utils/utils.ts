bea9f508d61d645604071a0a31ab6c0797d5b6e3
fowlerro <fwlerro@gmail.com>
2022-01-28 14:40:07 +0100

    Added unit tests for utils.ts, 100% coverage

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,8 +1,7 @@
-import { ChannelMention, ExcludeEnum, Guild, MemberMention, Snowflake } from "discord.js";
+import { ExcludeEnum } from "discord.js";
 import { ActivityTypes } from "discord.js/typings/enums";
 import path from "path";
 import fs from 'fs'
-import BaseCommand from "./structures/BaseCommand";
 
 export const botOwners = ["313346190995619841"];
 
@@ -22,56 +21,7 @@ export const palette: IPalette = {
     error: '#B71E13',
 }
 
-export function mapToObject(map: Map<any, any>) {
-    const out = Object.create(null)
-    map.forEach((value, key) => {
-      if(value instanceof Map) {
-        out[key] = mapToObject(value)
-      } else out[key] = value;
-    })
-    if(!out) return {}
-    return out
-}
-
-export function daysInMonth(month: number, year: number) {
-    return new Date(year, month, 0).getDate();
-}
-
-export function msToTime(ms: number) {
-    let result = ""
-    let d = Math.floor((ms/(1000*60*60*24)) % 7);
-    d && (result += d+'d '); 
-    let h = Math.floor((ms/(1000*60*60)) % 24);
-    h && (result += h+'h '); 
-    let m = Math.floor((ms/(1000*60)) % 60);
-    m && (result += m+'m '); 
-    let s = Math.floor((ms/(1000)) % 60);
-    s && (result += s+'s '); 
-    return result.trimEnd();
-}
-
-export function toggleBot(state: boolean) {
-    if(!state) client.isOnline = !client.isOnline;
-    if(state) client.isOnline = state;
-
-    if(client.isOnline) {
-        client.user?.setPresence({
-            status: 'online',
-            activities: [{
-                name: client.customActivity?.name,
-                type: client.customActivity?.type
-            }]
-        });
-    }
-    if(!client.isOnline) {
-        client.user?.setPresence({
-            status: 'invisible'
-        });
-    }
-    return client.isOnline;
-}
-
-export function setActivity(type: ExcludeEnum<typeof ActivityTypes, "CUSTOM">, activity: string) {
+export function setActivity(type: ExcludeEnum<typeof ActivityTypes, "CUSTOM">, activity?: string) {
     if(!type) return;
     client.customActivity = {
         name: activity,
@@ -85,23 +35,6 @@ export function setActivity(type: ExcludeEnum<typeof ActivityTypes, "CUSTOM">, a
     })
 }
 
-export async function getUserFromMention(mention: MemberMention | Snowflake) {
-    if(!mention) return false;
-    if(!isNaN(+mention)) return client.users.fetch(mention).catch(() => {});
-    const matches = mention.match(/^<@!?(\d+)>$/);
-    if(!matches) return false;
-
-    return client.users.fetch(matches[1]);
-}
-
-export async function getChannelFromMention(guild: Guild, mention: ChannelMention) {
-    if(!mention) return false
-    const matches = mention.match(/^<#(\d+)>$/);
-    if(!matches) return false;
-
-    return guild.channels.fetch(matches[1]).catch(err => console.error(err))
-}
-
 export function assignNestedObjects(obj: any, keyPath: string[], value: any) {
     const lastKeyIndex = keyPath.length-1;
     for (let i = 0; i < lastKeyIndex; ++i) {
@@ -112,29 +45,19 @@ export function assignNestedObjects(obj: any, keyPath: string[], value: any) {
     if(obj[keyPath[lastKeyIndex]]) {
         if(obj[keyPath[lastKeyIndex]].includes(value)) return;
         obj[keyPath[lastKeyIndex]] = [value, ...obj[keyPath[lastKeyIndex]]]
-    } else obj[keyPath[lastKeyIndex]] = [value]
+    } else {
+        obj[keyPath[lastKeyIndex]] = [value]
+    }
 }
 
 export function capitalize(string: string) {
-    if (typeof string !== 'string') return ''
+    if (typeof string !== 'string') return string
     return string.charAt(0).toUpperCase() + string.slice(1)
 }
 
 export function convertLargeNumbers(number: number) {
-    return number >= 1000000 ? Math.floor((number / 1000000) * 10) / 10 + "M" :
+    return number >= 1000000 ? Math.floor((number / 1000000) * 100) / 100 + "M" :
         number >= 1000 ? Math.floor((number / 1000) * 10) / 10 + "K" : number
-}
-
-export function compareArrayOfObjects(x: any, y: any): boolean {
-    if(x.length !== y.length) return false
-
-    for(const [index, obj] of x.entries()) {
-        for(const key in obj) {
-            if(x[index][key] != y[index][key]) return false
-        }
-    }
-
-    return true
 }
 
 export function getCommandCategories(): string[] {
