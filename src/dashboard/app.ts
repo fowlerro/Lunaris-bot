bd48dad41bf0da1143d163cabaecd0b8576e45ca
fowlerro <fwlerro@gmail.com>
2022-02-21 11:17:23 +0100

    fix method in previous commit

--- src/dashboard/app.ts
+++ src/dashboard/app.ts
@@ -44,7 +44,7 @@ export default async () => {
     app.use(passport.initialize());
     app.use(passport.session());
 
-    app.get('/', (req, res) => res.send(200))
+    app.get('/', (req, res) => res.sendStatus(200))
     app.use('/api', routes);
 
     app.listen(PORT, () => logger.info(`Running on Port ${PORT}`));
