3e0bc031935bc864b1d45bb1659719557f80ab20
fowlerro <fwlerro@gmail.com>
2022-04-09 17:10:06 +0200

    Clean old API file

--- src/dashboard/routes/index.ts
+++ src/dashboard/routes/index.ts
@@ -1,15 +1,12 @@
-import express from "express";
-import auth from "./auth";
-import guilds from "./guilds";
-import profile from "./profile";
-// import discord from './discord'
+import express from 'express';
+import auth from './auth';
+import guilds from './guilds';
+import profile from './profile';
 
 const router = express.Router();
 
-router.use("/auth", auth);
-router.use("/guilds", guilds);
-router.use("/profile", profile);
-
-// router.use('/discord', discord);
+router.use('/auth', auth);
+router.use('/guilds', guilds);
+router.use('/profile', profile);
 
 export default router;
